a719732b4cf36df839bc1ce210cd5942ca196c8d
Phuong My <101578258+phuongmy1602@users.noreply.github.com>
2023-03-06 14:57:29 +0700

    adding sound and animation

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
+//Detecting Button Click
 for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    // alert("i got clicked!");
-    // console.log(this.innerHTML);
-    this.style.color = "white"
+    makeSounds(this.innerHTML);
+    addAnimation(this.innerHTML);
   });
 }
 
+//Detecting Keyboard Press
+document.addEventListener("keypress", function () {
+  makeSounds(event.key);
+  addAnimation(event.key);
+});
 
-// let audio = new Audio("sounds/tom-1.mp3");
-//     audio.play();
+function makeSounds(key) {
+  switch (key) {
+    case "w":
+      let audio1 = new Audio("sounds/tom-1.mp3");
+      audio1.play();
+      break;
+    case "a":
+      let audio2 = new Audio("sounds/tom-2.mp3");
+      audio2.play();
+      break;
+    case "s":
+      let audio3 = new Audio("sounds/tom-3.mp3");
+      audio3.play();
+      break;
+    case "d":
+      let audio4 = new Audio("sounds/tom-4.mp3");
+      audio4.play();
+      break;
+    case "j":
+      let audio5 = new Audio("sounds/snare.mp3");
+      audio5.play();
+      break;
+    case "k":
+      let audio6 = new Audio("sounds/crash.mp3");
+      audio6.play();
+      break;
+    case "l":
+      let audio7 = new Audio("sounds/kick-bass.mp3");
+      audio7.play();
+      break;
+    default:
+      console.log("hello");
+  }
+}
+
+//Adding Animation
+
+function addAnimation(key) {
+  let activeKey = document.querySelector("." + key);
+  activeKey.classList.add("pressed");
+  setTimeout(function () {
+    activeKey.classList.remove("pressed");
+  }), 100;
+}
